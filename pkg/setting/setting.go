f4c896dd3b86a921d8c66be715d7c5579a24d6a2
770863472@qq.com <770863472@qq.com>
2019-06-24 10:39:12 +0800

    setting

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -2,28 +2,42 @@ package setting
 
 import (
 	"github.com/go-ini/ini"
-	"time"
 	"log"
+	"time"
 )
 
 var (
-	Cfg *ini.File
-	RunMode string
-	HttpPort int
-	ReadTimeout time.Duration
+	Cfg          *ini.File
+	RunMode      string
+	HttpPort     int
+	ReadTimeout  time.Duration
 	WriteTimeout time.Duration
-	PageSize int
-	JwtSectet string
+	PageSize     int
+	JwtSectet    string
 )
-func init(){
+
+func init() {
 	var err error
-	Cfg,err = ini.Load("conf/app.ini")
-	if(err !=nil){
-		log.Fatal("fail to parse 'conf.app.ini:%v'",err)
+	Cfg, err = ini.Load("conf/app.ini")
+	if err != nil {
+		log.Fatal("fail to parse 'conf.app.ini:%v'", err)
 	}
 	LoadBase()
 
 }
-func LoadBase(){
+func LoadBase() {
 	RunMode = Cfg.Section("").Key("RUN_MODE").MustString("debug")
+}
+func LoadService() {
+	ser, err := Cfg.GetSection("Server")
+	if err != nil {
+		log.Fatalf("faile to get section 'server':%v", err)
+	}
+	Runmode = Cfg.Section("").Key("Run_Mode").MustString("debug")
+	HttpPort = ser.Key("Http_Port").MustInt(8080)
+	ReadTimeout = time.Duration(ser.Key("READ_TIMEOUT").MustInt(60)) * time.Second
+	WriteTimeout = time.Duration(ser.Key("WRITE_TIMEOUT").MustInt(60)) * time.Second
+}
+func loadApp() {
+
 }
